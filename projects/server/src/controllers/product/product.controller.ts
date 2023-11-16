91c87d9fb30beb3b18ea90bf2551437395b969b5
Galih Setyawan <galih.kamvax032@gmail.com>
2023-11-16 08:17:17 +0700

    add: example folder of product module

---
+++ projects/server/src/controllers/product/product.controller.ts
@@ -0,0 +1,9 @@
+import ProductService from "../../service/products/product.service";
+
+export default class ProductController {
+  productService: ProductService;
+
+  constructor() {
+    this.productService = new ProductService();
+  }
+}
