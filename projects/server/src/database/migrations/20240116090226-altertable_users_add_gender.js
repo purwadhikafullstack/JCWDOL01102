ea2dfc21820001a3d1dad1df269d28269dfb80f7
Galih Setyawan <galih.kamvax032@gmail.com>
2024-01-16 16:58:40 +0700

    Add gender field to user model and migration

---
+++ projects/server/src/database/migrations/20240116090226-altertable_users_add_gender.js
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('users', 'gender', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('users', 'gender');
+  },
+};
