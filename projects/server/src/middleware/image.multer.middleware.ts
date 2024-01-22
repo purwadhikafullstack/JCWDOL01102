eeb314f681d9d0f350a570fce4bebfe0816a184d
Galih Setyawan <75205083+Nawaytes@users.noreply.github.com>
2024-01-22 13:56:25 +0700

    Development (#58)
    * add boilerplate express ts
    
    * modify: project name
    
    * add: rule prettier & eslint
    
    * add: example folder of product module
    
    * apply prettier & eslint
    
    * delete default migration
    
    * add rule no-unused-vars
    
    * add migration finpro
    
    * add dependency express validator
    
    * add api configs
    
    * feat : create user
    
    * add delete config
    
    * feat : user creation and document creation
    
    * alter table image_id nullable
    
    * fix baseModel reference
    
    * fix route name
    
    * fix baseModeConfig
    
    * fix: route & users model
    
    * add api create address & master data province
    
    * add api master data cities
    
    * add address api
    
    * fix: bug update address by id
    
    * feat : verify user eith email
    
    * feat : verify user eith email
    
    * feat : verification
    
    * feat : user verification
    
    * feat : log in adn private route
    
    * feat : login and jwt token
    
    * add api upload document & get document by uniqId
    
    * add product api
    
    * remove image after create product
    
    * hotflix : "jwt token"
    
    * revision : "service revision"
    
    * add category api
    
    * "feat : Dashboard"
    
    * feat : admin dashboard
    
    * add category api
    
    * fix optioal paginate params
    
    * update req.user after auth middleware
    
    * feat :
    
    * feat : Admin Dashboard
    
    * feat : Admin Dashboard
    
    * remove console.log
    
    * feat : add edit branch admin
    
    * merge
    
    * feat : report page
    
    * fix update product with transaction
    
    * new
    
    * feat : Voucher creation page
    
    * refactor:route
    
    * add docker file
    
    * add running script
    
    * fix wrong interface
    
    * bypas root api
    
    * add order module
    
    * fix baypass api external
    
    * feat : discount display
    
    * feat : voucher management page and login
    
    * order stuff api
    
    * feat : Landing Page and Navbar
    
    * feat : categories and product details
    
    * feat : Explore Page
    
    * feat:cart
    
    * Add order status "done" and update order status on payment success
    
    * feat : update branch id
    
    * Refactor payment code in OrderService
    
    * Add lodash library and update package dependencies
    
    * Add cron job to deliver orders every minute
    
    * Refactor cron job scheduling and order status handling
    
    * Add getBranchByUserId method to BranchController
    
    * Fix product price and stock validation issues
    
    * Fix order validation and stock check issues
    
    * Refactor DokuVAService and OrderService
    
    * Refactor payment code in OrderService
    
    * Update CronJob class to include new order status transitions
    
    * feat : order
    
    * hotflix : permission
    
    * Refactor DokuVAService to use shorthand property syntax for amount, name, and email
    
    * Add getNotification route and method to OrderController and OrderRouter
    
    * Refactor console.log statements
    
    * Remove unused variables in getDetailById method
    
    * Add gender field to user model and migration
    
    * Update dotenv configuration paths
    
    * hotflix : order detail and several change
    
    * Refactor fileFilter in image.multer.middleware.ts to use toLowerCase() for file extension check
    
    * hotflix : revision on order update
    
    * Update path to .env file in config files
    
    * fixing path upload multer
    
    * add upload folder
    
    ---------
    
    Co-authored-by: Naufan Rikza Ahmada <65270440+NaufanRikza@users.noreply.github.com>
    Co-authored-by: Enginoir <124773538+enginoir@users.noreply.github.com>
    Co-authored-by: NaufanRikza <deathriver26@gmail.com>
    Co-authored-by: Bryan Karta <karta.bryan@gmail.com>

--- projects/server/src/middleware/image.multer.middleware.ts
+++ projects/server/src/middleware/image.multer.middleware.ts
@@ -1,15 +1,16 @@
+import { HttpStatusCode } from 'axios';
 import { NextFunction, Request, Response } from 'express';
 import multer from 'multer';
 import { extname } from 'path';
 
 const upload = multer({
-  dest: 'uploads/',
+  dest: '../../uploads/',
   limits: {
     fileSize: 1024 * 1024 * 1,
   },
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads/');
+      cb(null, '../../uploads/');
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
@@ -28,7 +29,10 @@ const upload = multer({
 export function multerMiddleware(req: Request, res: Response, next: NextFunction) {
   upload.single('file')(req, res, function (error: any) {
     if (error) {
-      return res.status(400).json({ message: error.message });
+      return res.status(400).json({
+        httpCode: HttpStatusCode.BadRequest,
+        message: error.message,
+      });
     }
     next();
   });
